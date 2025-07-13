ee88b4d8c8de1b44e60b924fcc669d8a9f8c5a47
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2025-07-13 12:57:58 +0530

    added onRampTransactions backend

--- apps/bank-webhook/index.ts
+++ apps/bank-webhook/index.ts
@@ -7,11 +7,41 @@ const app = express();
 
 app.use(express.json())
 
-app.post("/hdfcwebhook", (req,res)=>{
+app.post("/hdfcwebhook",async  (req,res)=>{
+    try {
     const paymentInformation = {
         token:req.body.token,
         userId:req.body.user_identifier,
         amount:req.body.amount
     }
-    db.balance
+    await db.balance.update({
+        where:{
+            userId:paymentInformation.userId
+        },
+        data:{
+            amount:{
+                increment:paymentInformation.amount
+            }
+        }
+    })
+
+    await db.onRampTransaction.update({
+        where:{
+            token:paymentInformation.token,
+        },
+        data:{
+            status:"Success"
+        }
+    })
+    res.status(200).json({
+        message:"captured"
+    })
+
+    } catch (e) {
+        console.error(e);
+        res.status(411).json({
+            message: "Error while processing webhook"
+        })
+    }
+
 })
